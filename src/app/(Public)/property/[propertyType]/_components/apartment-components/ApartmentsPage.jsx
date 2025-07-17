db0caba57e2998239342b82d6bcf6d3c919c8e62
asifsheikh1 <nmasifsheikh2003@gmail.com>
2025-07-17 11:06:17 +0600

    refactor: Improve layout and responsiveness in ApartmentsContainer, ApartmentsPage, and HorizontalPropertyCard components

--- src/app/(Public)/property/[propertyType]/_components/apartment-components/ApartmentsPage.jsx
+++ src/app/(Public)/property/[propertyType]/_components/apartment-components/ApartmentsPage.jsx
@@ -104,8 +104,8 @@ export default function ApartmentsPage() {
         searchedGuests={guests}
       />
 
-      <ResponsiveContainer className="flex-start-between mt-16 gap-x-14">
-        <div className="w-1/4">
+      <ResponsiveContainer className="flex flex-col lg:flex-row flex-start-between mt-16 lg:gap-x-14 gap-y-12 lg:gap-y-0">
+        <div className="w-full lg:w-1/4">
           <ApartmentFilters
             priceRange={priceRange}
             setPriceRange={setPriceRange}
